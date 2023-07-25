7ce1d3d33287e8ffed41dba88baf7cb1403a3d29
Dmytro <moiseenkodmitriy@i.ua>
2023-07-25 16:43:53 +0300

    sdfsf

--- js/modal.js
+++ js/modal.js
@@ -2,11 +2,13 @@
   const refs = {
     openModalBtn: document.querySelector('[data-modal-open]'),
     closeModalBtn: document.querySelector('[data-modal-close]'),
+    closeModalBtn2: document.querySelector('[data-modal-close2]'),
     modal: document.querySelector('[data-modal]'),
   };
 
   refs.openModalBtn.addEventListener('click', toggleModal);
   refs.closeModalBtn.addEventListener('click', toggleModal);
+  refs.closeModalBtn2.addEventListener('click', toggleModal);
 
   function toggleModal() {
     refs.modal.classList.toggle('is-hidden');
