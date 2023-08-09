4c31040a4fb2ccbe8c4d1232fe00688e99bd5748
Dmytro1117 <111579384+Dmytro1117@users.noreply.github.com>
2023-08-09 17:45:32 +0000

    Deploying to gh-pages from @ Dmytro1117/web-studio@ca83aace6fab22beba3c1bcba8f93b71d871b81f 🚀

---
+++ index.1def86ee.js
@@ -0,0 +1,2 @@
+(()=>{let e=document.querySelector(".mobile-menu__btn"),o=document.querySelector(".mobile-menu");e.addEventListener("click",function(){e.classList.toggle("is-open"),o.classList.toggle("is-open"),document.body.classList.toggle("modal-open")})})();
+//# sourceMappingURL=index.1def86ee.js.map
